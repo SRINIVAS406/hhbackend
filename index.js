18e75710439b23fcbc6af58d2cb223b62a12abc2
Srinivas Reddy <srinivasreddy@Srinivass-MacBook-Air.local>
2024-01-30 16:54:47 +0530

    cors updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
 
 mongoose.connect('mongodb+srv://sri:sri@hhdev.amtukyd.mongodb.net/?retryWrites=true&w=majority');
 const db = mongoose.connection;
